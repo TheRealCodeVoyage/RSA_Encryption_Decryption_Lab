ad62b71d7fb7fd350d75b7cb04a7fe8a8ddf5bef
TheRealCodeVoyage <nulljuju.dev@gmail.com>
2024-10-02 14:27:46 -0700

    update and document the code

--- rsa.c
+++ rsa.c
@@ -12,18 +12,21 @@ void printBN(char *msg, BIGNUM *a)
 }
     int main()
     {
-        BN_CTX *ctx = BN_CTX_new();
-        BIGNUM *m = BN_new();
-        BIGNUM *e = BN_new();
-        BIGNUM *n = BN_new();
-        BIGNUM *d = BN_new();
-        BIGNUM *enc = BN_new();
-        BIGNUM *dec = BN_new();
-        BIGNUM *enc_key = BN_new();
-        BIGNUM *dec_key = BN_new();
+        BN_CTX *ctx = BN_CTX_new(); // Context
+        BIGNUM *m = BN_new();   //  Plaintext
+        BIGNUM *e = BN_new();   //  Alice's Public Key
+        BIGNUM *n = BN_new();   //  Alice's Modulus (part of public key)
+        BIGNUM *d = BN_new();   //  Alice's Private Key
+        BIGNUM *enc = BN_new(); //  Encrypted Message
+        BIGNUM *dec = BN_new(); //  Decrypted Message
+        BIGNUM *enc_key = BN_new(); //  Encryption Key
+        BIGNUM *dec_key = BN_new(); //  Decryption Key
+        BIGNUM *hash = BN_new();    //  SHA256 hash of the message
+        BIGNUM *signature = BN_new();   //  The RSA signature
 
         // Initialize e,n,d
         //e is used as public key
+        //n is modulus
         //d is used as private key
         BN_hex2bn(&e, "010001");
         BN_hex2bn(&n, "DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5");
@@ -35,19 +38,60 @@ void printBN(char *msg, BIGNUM *a)
         //Uncomment next line and Initialize encrypted text (ciphertext) enc if needed
         //BN_hex2bn(&enc, "");
 
-        //Encryption Formula: m^enc_key mod n
+        //  Encryption Formula: 
+        //  Encrypted Message = Plaintext ^ Encryption Key mod Modulus
+        //  enc = m ^ enc_key mod n
 
         //Uncomment next 3 lines and Initialize the encryption key based on the task
         //enc_key = ;
         //BN_mod_exp(enc,m,enc_key,n,ctx);
         //printBN("Encrypted message = ", enc);
     
-        //Decryption Formula: enc^dec_key mod n
+        //  Decryption Formula:
+        //  Decrypted Message = Encrypted Message ^ Decryption Key mod Modulus 
+        //  dec = enc ^ dec_key mod n
 
-        //Uncomment next 3 lines andInitialize the decryption key based on the task
+
+        //Uncomment next 3 lines and Initialize the decryption key based on the task
         //dec_key = ;
         //BN_mod_exp(dec,enc,dec_key,n,ctx);
         //printBN("Decrypted message = ", dec);
 
+        //  RSA Signature Formula:
+        //  Signature = Hashed Message ^ Private Key mod Modulus
+        //  signature = hash ^ d mod n
+
+        //Uncomment next 3 lines to Sign a message with Alice's Private Key
+        //BN_hex2bn(&hash, "insert_hash_value_of_the_message_here");
+        //BN_mod_exp(signature, hash, d, n, ctx);
+        //printBN("Signature: ", signature);
+
+
+        //  RSA Signature Verification Formula:
+        //  Plaintext (in hex) = Signature ^ Public Key mod Modulus
+        //  m = signature ^ e mod n
+
+        //Uncomment next 4 lines to Verify the signature of Alice using their Public Key
+        //BN_hex2bn(&hash, "insert_hash_value_of_the_message_here");
+        //BN_hex2bn(&signature, "signature_given_in_task_4");
+        //BN_mod_exp(m, sig, e, n, ctx);
+        //printBN("Recovered hash from signature (in hex):", m);
+
+        
+        
+        
+        
+        //  Free allocated memory
+        BN_free(d);
+        BN_free(m);
+        BN_free(e);
+        BN_free(n);
+        BN_free(enc);
+        BN_free(enc_key);
+        BN_free(dec);
+        BN_free(dec_key);
+        BN_free(hash);
+        BN_free(signature);
+        BN_CTX_free(ctx);
         return 0;
     }
